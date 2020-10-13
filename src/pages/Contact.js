f67e9b92c57136f0a19c38172cb34e9643309895
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-10-13 19:21:16 +0200

    contact page done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactGa from 'react-ga';
+import emailjs from 'emailjs-com'
 
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
@@ -12,18 +13,53 @@ class Contact extends React.Component {
 
 	constructor(props){
 		super(props);
-		this.state = {
-		}
+		this.sendEmail = this.sendEmail.bind(this);
 	}
 
 	componentDidMount() {
 		ReactGa.pageview('/contact')
 	}
 
+	sendEmail(e) {
+		e.preventDefault();
+		emailjs.sendForm('gmail', 'template_dr88k0s', e.target, 'user_zBglHhqUaQ7omCp7GvAHz')
+			.then((result) => {
+				alert("Bien reçu nous reviendrons vers vous");
+			}, (error) => {
+				alert(error.text);
+			});
+		e.target.reset()
+		ReactGa.event({
+			category:'Button',
+			action: 'Sent contact mail'
+		})
+	}
+
     render() {
         return (
           <Container>
-          coucou
+			<Form onSubmit={this.sendEmail} varient="info">
+			  <Form.Group controlId="ControlInput">
+			    <Form.Label>Addresse mail</Form.Label>
+			    <Form.Control required type="email" name="reply_to" placeholder="name@example.com" />
+			  </Form.Group>
+			  <Form.Group controlId="ControlInput2">
+			    <Form.Label>Nom</Form.Label>
+			    <Form.Control required type="text" name="name" placeholder="Claude" />
+			  </Form.Group>
+			  <Form.Group controlId="ControlInput3">
+			    <Form.Label>Objet</Form.Label>
+			    <Form.Control required type="text" name="subject" placeholder="Super site, comment puis-je vous remercier d'avoir rendue ma vie meilleure ?" />
+			  </Form.Group>
+			  <Form.Group controlId="ControlTextarea1">
+			    <Form.Label>Message</Form.Label>
+			    <Form.Control required as="textarea" name="message" rows={3} />
+			  </Form.Group>
+			  <Button variant="info" type="submit" className="justify-content-md-center mx-auto">
+				Envoyer
+			  </Button>
+			</Form>
+
 		  </Container>
 		);
 	}
