8dd6ec37a82bd4edff8e92ae059f6aca2ad50ecf
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-10-16 18:28:13 +0200

    Updates

---
+++ precache-manifest.f6b167eac2ffb8c62ed46b37333e5a7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcaec7ba6171f0bae09479a8f864660",
+    "url": "/koh-lantah-simulator/index.html"
+  },
+  {
+    "revision": "db48cbf613610005e2dc",
+    "url": "/koh-lantah-simulator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a1ae60a89d4309de3656",
+    "url": "/koh-lantah-simulator/static/css/main.aeaa6e53.chunk.css"
+  },
+  {
+    "revision": "db48cbf613610005e2dc",
+    "url": "/koh-lantah-simulator/static/js/2.ccbeb8eb.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/koh-lantah-simulator/static/js/2.ccbeb8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ae60a89d4309de3656",
+    "url": "/koh-lantah-simulator/static/js/main.22f02c6a.chunk.js"
+  },
+  {
+    "revision": "48c7c5aa1055a529ddca",
+    "url": "/koh-lantah-simulator/static/js/runtime-main.f6ab98ee.js"
+  }
+]);
