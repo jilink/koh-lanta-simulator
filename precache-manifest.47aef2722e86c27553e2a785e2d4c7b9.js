da28140781fec8814931f55072439226ee3618c2
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-10-16 19:26:42 +0200

    Updates

---
+++ precache-manifest.47aef2722e86c27553e2a785e2d4c7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7a64a4f4e70a09a6b16769eaa55b43",
+    "url": "/koh-lanta-simulator/index.html"
+  },
+  {
+    "revision": "86ce5e6e0b15c3ff052b",
+    "url": "/koh-lanta-simulator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8eefed05eb25974ebcf7",
+    "url": "/koh-lanta-simulator/static/css/main.aeaa6e53.chunk.css"
+  },
+  {
+    "revision": "86ce5e6e0b15c3ff052b",
+    "url": "/koh-lanta-simulator/static/js/2.bfee488f.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/koh-lanta-simulator/static/js/2.bfee488f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eefed05eb25974ebcf7",
+    "url": "/koh-lanta-simulator/static/js/main.34f57195.chunk.js"
+  },
+  {
+    "revision": "b9d85ad12d8e06873dc2",
+    "url": "/koh-lanta-simulator/static/js/runtime-main.8f77982b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/koh-lantah-simulator/precache-manifest.9c1e265a54e4eb569881cae40b3c8004.js"
+  "/koh-lanta-simulator/precache-manifest.47aef2722e86c27553e2a785e2d4c7b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/koh-lantah-simulator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/koh-lanta-simulator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
